36d47fc3a72adc734d615aaccf4241e4d4db95a5
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-03-12 17:45:55 -0500

    basic views and routing

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,1 @@
-//todo
+//todo copy to public
--- server.js
+++ server.js
@@ -2,10 +2,26 @@ var koa = require('koa'),
   common = require('koa-common'),
   http = require('http'),
   //https = require('https'),
+  route = require('koa-route'), // todo koa-router?
+  render = require('./lib/render'),
+  serve = require('koa-static'),
   app = koa();
 
 app.use(common.logger());
 app.use(common.responseTime());
+
+app.use(serve('bower_components'));
+
+app.use(route.get('/', home));
+app.use(route.get('/calendar', calendar));
+
+function *home() {
+  this.body = yield render('index', {});
+}
+
+function *calendar() {
+  this.body = yield render('calendar', {});
+}
 
 app.use(function *(){
   this.body = 'Hello World';
@@ -13,3 +29,4 @@ app.use(function *(){
 
 http.createServer(app.callback()).listen(3000);
 //https.createServer(app.callback()).listen(3001);
+console.log('listening on port 3000');
